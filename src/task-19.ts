a712f14eac57262a4aab52538e85754db7924cb2
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-07-19 12:29:46 +0300

    Update task-19.ts

--- src/task-19.ts
+++ src/task-19.ts
@@ -3,27 +3,27 @@
 
 
 
-// import axios from "axios";
+import axios from "axios";
 
-// interface User{
-//     id: number;
-//     name: string;
-//     username: string;
-// }
+interface User{
+    id: number;
+    name: string;
+    username: string;
+}
 
-// const fetchUser = async (userId: number): Promise<User> => {
-//     const response = await axios.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`);
-//     console.log(response);
+const fetchUser = async (userId: number): Promise<User> => {
+    const response = await axios.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    console.log(response);
     
-//   return response.data;
-// };
+  return response.data;
+};
 
-// const getUserName = async (id: number) => {
-//   const user = await fetchUser(id);
-//   console.log(user.name);
-// };
+const getUserName = async (id: number) => {
+  const user = await fetchUser(id);
+  console.log(user.name);
+};
 
-// getUserName(1);
+getUserName(1);
 
 
 
