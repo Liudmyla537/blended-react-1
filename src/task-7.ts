07eae292697a1fcca4770cb87b7268ec17133ff6
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-07-19 12:23:18 +0300

    Update task-7.ts

--- src/task-7.ts
+++ src/task-7.ts
@@ -1,14 +1,14 @@
-// interface User{
-//     name: string;
-//     age: number;
-// }
+interface User{
+    name: string;
+    age: number;
+}
 
-// const users: User[] = [
-//   { name: "Alice", age: 30 },
-//   { name: "Bob", age: 25 },
-// ];
+const users: User[] = [
+  { name: "Alice", age: 30 },
+  { name: "Bob", age: 25 },
+];
 
-// console.log(users.push({ name: "Petya", age: 35 }));
+console.log(users.push({ name: "Petya", age: 35 }));
 
 
 // **************************************
