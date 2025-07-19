179351f2a9ca08a588773c9c5fbb2bf06c5d4ff5
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-07-19 12:29:05 +0300

    Update task-16.ts

--- src/task-16.ts
+++ src/task-16.ts
@@ -9,22 +9,22 @@
 
 
 
-// interface Profile {
-//     username: string;
-//     age: number;
-//     isAdmin: boolean;
-// }
+interface Profile {
+    username: string;
+    age: number;
+    isAdmin: boolean;
+}
 
 
-// function fetchProfile(): Promise<Profile> {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve({ username: "max_123", age: 28, isAdmin: true });
-//     }, 1000);
-//   });
-// }
+function fetchProfile(): Promise<Profile> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ username: "max_123", age: 28, isAdmin: true });
+    }, 1000);
+  });
+}
 
-// fetchProfile().then(profile => console.log(profile));
+fetchProfile().then(profile => console.log(profile));
 
 
 
