7e3540d4f572c51f1c73a96ed79704f716a5315e
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-07-19 12:20:31 +0300

    Update task-2.ts

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,23 +1,24 @@
 // Є функція, яка приймає суму (число) та тип валюти.
 
-
-// type Currency = "USD" | "EUR" | "UAH";
-
-// interface ConvertCurrency {
-//     amount: number;
-//     currency: Currency;
-// }
-
-// function convertCurrency({ amount, currency }: ConvertCurrency): void {
-//   console.log(`Converting ${amount} to ${currency}`);
-// };
-
-// convertCurrency({ amount: 100, currency: "UAH" });
-
-
-
-
 // Завдання:
 
 // Типізуйте параметри функції дозволивши властивості currency лише одне із значень "USD", "EUR", "UAH".
 // Типізуйте повернення функції.
+
+
+type Currency = "USD" | "EUR" | "UAH";
+
+interface ConvertCurrency {
+    amount: number;
+    currency: Currency;
+}
+
+function convertCurrency({ amount, currency }: ConvertCurrency): void {
+  console.log(`Converting ${amount} to ${currency}`);
+};
+
+convertCurrency({ amount: 100, currency: "UAH" });
+
+
+
+
