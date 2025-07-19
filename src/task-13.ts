e3ecd4ef83a80b12a81c514e9774eace88e7787d
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-07-19 12:28:06 +0300

    Update task-13.ts

--- src/task-13.ts
+++ src/task-13.ts
@@ -2,25 +2,25 @@
 
 
 
-// type Action = {
-//     type: "increment" | "decrement";
-// };
+type Action = {
+    type: "increment" | "decrement";
+};
 
-// function reducer(state: number, action: Action): number {
-//   switch (action.type) {
-//     case "increment":
-//       return state + 1;
-//     case "decrement":
-//       return state - 1;
-//     default:
-//       return state;
-//   }
-// }
+function reducer(state: number, action: Action): number {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+}
 
-// const resultState = 2;
+const resultState = 2;
 
-// console.log(reducer(resultState, { type: "increment" }));
-// console.log(reducer(resultState, { type: "decrement" }));
+console.log(reducer(resultState, { type: "increment" }));
+console.log(reducer(resultState, { type: "decrement" }));
 
 
 
