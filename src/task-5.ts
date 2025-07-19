7aac31ffd51e2044cb9e7dc5beb044b359367cf1
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-07-19 12:22:40 +0300

    Update task-5.ts

--- src/task-5.ts
+++ src/task-5.ts
@@ -1,29 +1,29 @@
-// interface User {
-//   name: string;
-//   age: number;  
-// }
+interface User {
+  name: string;
+  age: number;  
+}
 
 
-// interface CreateUser {
-//     name: string;
-//     age: number;
-//     isAdmin: boolean;
-// }
+interface CreateUser {
+    name: string;
+    age: number;
+    isAdmin: boolean;
+}
 
-// function createUser({ name, age }: User): CreateUser {
-//   return {
-//     name,
-//     age,
-//     isAdmin: false
-//   };
-// }
+function createUser({ name, age }: User): CreateUser {
+  return {
+    name,
+    age,
+    isAdmin: false
+  };
+}
 
-// createUser({ name: "Alice", age: 30 });
-// console.log(createUser({ name: "Alice", age: 30 }));
+createUser({ name: "Alice", age: 30 });
+console.log(createUser({ name: "Alice", age: 30 }));
 
 
 
-// ******************************************************
+
 // Завдання:
 
 // Типізуйте функцію повністю: параметри і повернення функції.
