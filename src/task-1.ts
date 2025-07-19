9b19e7439ce498be30e7450397751efb5d8523f3
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-07-19 12:20:03 +0300

    Update task-1.ts

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,22 +1,23 @@
 // Є об'єкт settings, який містить налаштування.
 
-// type Settings = {
-//     darkMode: boolean;
-//     fontSize: number;
-//     language: string;
-// }
-
-// const settings: Settings = {
-//   darkMode: true,
-//   fontSize: 16,
-//   language: "en",
-// };
-
-// console.log(settings);
-
-
-
 // Завдання:
 
 // Створіть тип Settings, який описує цей об'єкт.
 // Типізуйте settings через цей тип.
+
+type Settings = {
+    darkMode: boolean;
+    fontSize: number;
+    language: string;
+}
+
+const settings: Settings = {
+  darkMode: true,
+  fontSize: 16,
+  language: "en",
+};
+
+console.log(settings);
+
+
+
