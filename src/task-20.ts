0994c2b29a167eeb1eea940dd857869db218ff59
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-07-19 12:30:15 +0300

    Update task-20.ts

--- src/task-20.ts
+++ src/task-20.ts
@@ -2,41 +2,34 @@
 
 // Функція fetchPosts повинна отримати список постів з API за допомогою бібліотеки axios.
 
+import axios from "axios";
 
+interface Post{
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
 
-
-// import axios from "axios";
-
-// interface Post{
-//     userId: number;
-//     id: number;
-//     title: string;
-//     body: string;
-// }
-
-// const fetchPosts = async (): Promise<Post[]> => {
-//     const response = await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
-//     // console.log(response);
+const fetchPosts = async (): Promise<Post[]> => {
+    const response = await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
+    // console.log(response);
     
-//   return response.data;
-// };
+  return response.data;
+};
 
-// const logThreePosts = (posts: Post[]): void => {
-//     posts.slice(0, 3).forEach(post => {
-//         console.log(`Title: ${post.title}`);
-//         console.log(`Body: ${post.body}`);
+const logThreePosts = (posts: Post[]): void => {
+    posts.slice(0, 3).forEach(post => {
+        console.log(`Title: ${post.title}`);
+        console.log(`Body: ${post.body}`);
         
-//     })
-// }
+    })
+}
 
-// fetchPosts()
-//     .then(posts => {
-//         logThreePosts(posts);  
-//     })
-
-
-
-
+fetchPosts()
+    .then(posts => {
+        logThreePosts(posts);  
+    })
 
 
 // Завдання:
