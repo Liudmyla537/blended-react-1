10e27ac063c9309aacf012754479956ef7fe6d93
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-07-19 12:23:46 +0300

    Update task-8.ts

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,27 +1,27 @@
-// enum Role {
-//   Admin,
-//   User,
-//   Guest,
-// }
+enum Role {
+  Admin,
+  User,
+  Guest,
+}
 
-// function getPermissions(role: Role): string[] {
-// // console.log(role);
+function getPermissions(role: Role): string[] {
+// console.log(role);
 
 
-//     switch (role) {
-//         case Role.Admin:
-//             return ["create", "read", "update", "delete"];
-//         case Role.User:
-//             return ["read", "update"];
-//         case Role.Guest:
-//             return ["read"];
-//         default:
-//             return [];
-//     }
-// }
+    switch (role) {
+        case Role.Admin:
+            return ["create", "read", "update", "delete"];
+        case Role.User:
+            return ["read", "update"];
+        case Role.Guest:
+            return ["read"];
+        default:
+            return [];
+    }
+}
 
-// console.log(getPermissions(Role.Admin));
-// console.log(getPermissions("Admin"));
+console.log(getPermissions(Role.Admin));
+console.log(getPermissions("Admin"));
 
 
 //***************************************************** */
